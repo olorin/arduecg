fe30c4cd353cd608eb9a38ff4d2b899a09a8af53
Sharif Olorin <sio@tesser.org>
2015-02-07 07:02:19 +0000

    Repair tab damage

--- src/bits.c
+++ src/bits.c
@@ -11,7 +11,7 @@ uint16_t swap_endian_16(uint16_t n)
 uint32_t swap_endian_32(uint32_t n)
 {
 	return (n & 0x000000FFU) << 24 |
-               (n & 0x0000FF00U) << 8  |
+	       (n & 0x0000FF00U) << 8  |
 	       (n & 0x00FF0000U) >> 8  |
 	       (n & 0xFF000000U) >> 24; 
 }
