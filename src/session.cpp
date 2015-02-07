9672f2bda23a58b2b80c36a1c851bd957f66f992
Sharif Olorin <sio@tesser.org>
2015-02-07 08:15:51 +0000

    Init session file

--- src/session.cpp
+++ src/session.cpp
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <SPI.h>
+#include <SD.h>
 
 #include "session.h"
 #include "bits.h"
@@ -20,4 +22,31 @@ void init_session_header(uint8_t* buf, uint64_t timestamp, uint16_t channels)
 	//      endianness-agnostic.
 	uint64_t be_ts = swap_endian_64(timestamp);
 	memcpy(&(buf[8]), &be_ts, 8);
+}
+
+int session_write(session *s, uint8_t *buf, size_t len)
+{
+	int written = s->fh.write(buf, len);
+	s->fh.flush();
+	return written;
+}
+
+session* init_session(uint64_t timestamp, uint16_t channels)
+{
+	session *s = (session*) malloc(sizeof(session));
+	if (s == NULL) {
+		return NULL;
+	}
+	uint8_t buf[SESSION_HEADER_SIZE];
+	init_session_header(buf, timestamp, channels);
+	char fname[33];
+	strncpy(fname, "ecg/session_", 12);
+	snprintf(fname+12, 21, "%llu", timestamp);
+	s->init_time = timestamp;
+	s->fh = SD.open(fname, FILE_WRITE);
+	if (!s->fh) {
+		return NULL;
+	}
+	session_write(s, buf, SESSION_HEADER_SIZE);
+	return s;
 }
--- src/session.h
+++ src/session.h
@@ -1,6 +1,18 @@
 #ifndef H_SESSION
 #define H_SESSION
 
+#include <SPI.h>
+#include <SD.h>
+
 #define SESSION_HEADER_SIZE 16
+
+typedef struct {
+	File fh;
+	uint64_t init_time;
+} session;
+
+session* session_init(uint64_t timestamp, uint16_t chans);
+
+int session_write(session *s, uint8_t *buf, size_t len);
 
 #endif
