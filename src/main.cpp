890f0b389baed15d20bf5b3a23b2d54e0deceb7f
Sharif Olorin <sio@tesser.org>
2015-02-07 10:49:28 +0000

    Add error state and debugging output

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,33 @@
 #define SD_CHIPSELECT 4
 #define SAMPLE_RATE 256
 
+#define PIN_ERROR_LED 13
+
+#define DEBUG 1
+
 uint8_t frame_buf[FRAME_SIZE];
 uint8_t frame_counter;
+
+void state_error()
+{
+	#ifdef DEBUG
+	Serial.println("Entering error state.");
+	#endif
+	pinMode(PIN_ERROR_LED, OUTPUT);
+	for (;;) {
+		digitalWrite(PIN_ERROR_LED, HIGH);
+		delay(500);
+		digitalWrite(PIN_ERROR_LED, LOW);
+		delay(500);
+	}
+}
+
+void state_run()
+{
+	for (;;) {
+		// do stuff
+	}
+}
 
 int init_sd() 
 {
@@ -25,11 +50,17 @@ int init_ecg()
 	frame_counter = 0;
 	memset(frame_buf, 0, FRAME_SIZE);
 	if (init_sd() < 0) {
+		#ifdef DEBUG
+		Serial.println("Could not initialize SD card.");
+		#endif
 		return -1;
 	}
 	uint64_t init_time = 1423297511ULL; // XXX: actually get time
 	session *s = session_init(init_time, FRAME_CHANNELS);
 	if (s == NULL) {
+		#ifdef DEBUG
+		Serial.println("Could not initialize session.");
+		#endif
 		return -2;
 	}
 	return 0;
@@ -37,12 +68,13 @@ int init_ecg()
 
 int main(void) 
 {
+	#ifdef DEBUG
+	Serial.begin(9600);
+	Serial.println("ecg-datalogger initializing.");
+	#endif
 	if (init_ecg() != 0) {
-		return -1;
-	}
-	for (;;) {
-		frame_buf[0] = frame_counter;
-		// Counter will overflow at 256.
-		frame_counter++;
+		state_error();
+	} else {
+		state_run();
 	}
 }
